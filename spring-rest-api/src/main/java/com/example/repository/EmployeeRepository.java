caae3eea895df5cfde4d17e9d8f73d2d5073e7a7
nazilealic <nazilealic@gmail.com>
2025-08-05 09:53:27 +0300

    ilk commit: spring-rest-api-projesi

---
+++ spring-rest-api/src/main/java/com/example/repository/EmployeeRepository.java
@@ -0,0 +1,29 @@
+package com.example.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.example.model.Employee;
+
+@Repository
+public class EmployeeRepository {
+	
+	@Autowired
+	private List<Employee> employeeList;
+
+	public List<Employee> getAllEmployeeList(){
+		return employeeList;
+	}
+	public Employee getEmployeeById(String id) {
+		Employee findEmployee = null;
+		for(Employee employee : employeeList) {
+			if(id.equals(employee.getId())) {
+				findEmployee = employee;
+				break;
+			}
+		}
+		return findEmployee;
+	}
+}
---
+++ spring-rest-api/src/main/java/com/example/services/EmployeeService.java
@@ -0,0 +1,31 @@
+package com.example.services;
+
+import java.util.List;
+import com.example.starter.SpringRestApiApplication;
+
+import jakarta.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.model.Employee;
+import com.example.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	public List<Employee> getAllEmployeeList(){
+		
+		return employeeRepository.getAllEmployeeList();
+	}
+	
+	public Employee getEmployeeById(String id) {
+		return employeeRepository.getEmployeeById(id);
+	}
+	
+}
